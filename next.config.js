836a3bd1f8478bb916c28616159f7b16cd15171e
MarlonManabat <rarer.niche-7f@icloud.com>
2025-08-13 23:05:07 +0800

    feat: add minimal API and redirect

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
     const target = process.env.API_BASE_URL;
     return target ? [{ source: '/api/:path*', destination: `${target}/:path*` }] : [];
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.quickgig.ph' }],
+        destination: 'https://quickgig.ph/:path*',
+        permanent: true,
+      },
+    ];
+  },
   eslint: { ignoreDuringBuilds: false },
   typescript: { ignoreBuildErrors: false },
 };
