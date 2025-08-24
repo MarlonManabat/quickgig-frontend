38ddda80c29c01c3cc19f9e968dc46318282ec54
MarlonManabat <rarer.niche-7f@icloud.com>
2025-08-24 04:39:48 -0700

    fix: use path matchers for static asset headers

--- next.config.js
+++ next.config.js
@@ -15,7 +15,8 @@ module.exports = withBundleAnalyzer({
   async headers() {
     return [
       {
-        source: '/(.*)\\.(?:js|css|woff2|png|jpg|jpeg|gif|svg|webp|avif)$',
+        source:
+          '/:path*{.js,.css,.woff2,.png,.jpg,.jpeg,.gif,.svg,.webp,.avif}',
         headers: [
           { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
         ],
